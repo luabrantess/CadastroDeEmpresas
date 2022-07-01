5df7b5c0272f122c4f63818d35dbe3345308d24a
Ana Luiza Borba de Abrantes <ana.abrantes@aeb.gov.br>
2022-07-01 12:47:27 -0300

    Iniciando um projeto

---
+++ emp/models.py
@@ -0,0 +1,42 @@
+from pyexpat import model
+from re import M
+from django.db import models 
+
+class Cidade(models.Model):
+    id = models.IntegerField(null=False, primary_key=True)
+    municipio = models.CharField(max_length=20, null=True)
+    uf = models.CharField(max_length=5, null=True)
+   
+
+class Area(models.Model):
+    id = models.IntegerField(primary_key=True, null= False)
+    area = models.CharField(max_length=20, null=False)
+
+
+
+class Empresa(models.Model):
+
+    id = models.IntegerField(null=False, primary_key=True)
+    nome = models.CharField(max_length=100, null=False)
+    cidade = models.TextField(null=False)
+    endereco = models.CharField(max_length=100, null=False)
+    descricao = models.TextField(null=False)
+    descricao_ingles = models.TextField(max_length=200, null=True)
+    slogan = models.TextField(max_length=100, null=True)
+    ponto_contato = models.CharField(max_length=50, null=True)
+ 
+    email = models.CharField(max_length=50, null=False)
+    site = models.CharField(max_length=50, null=True)
+    
+
+    def __str__(self):
+        return self.nome 
+
+
+class Produto(models.Model):
+    ident = models.ForeignKey('Empresa', null=False, on_delete=models.PROTECT)
+    produto = models.TextField(null=False)
+
+class Telefone(models.Model):
+    ident = models.ForeignKey('Empresa', null=False,on_delete=models.PROTECT)
+    telefone = models.BigIntegerField(null=False)
